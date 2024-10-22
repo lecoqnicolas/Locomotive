710480bfc359afa19019ccab3740a558eebaab80
Debian <debian@l40s-90-gra11-scalian-mce>
2024-10-22 13:25:44 +0000

    bleu score corrections

--- locomotive_llm/eval/_bleu_score.py
+++ locomotive_llm/eval/_bleu_score.py
@@ -18,10 +18,13 @@ def eval_text_bleu(translated_text, reference_text):
     Evaluate a text line by line using bleu score
     """
     reference = [[ref] for ref in reference_text.split("\n")]
+    #reference =  [ref  for ref in reference if len(ref[0])> 0]
     translated = translated_text.split("\n")
+    #translated =  [translation  for translation in translated if len(translation)> 0]
+    for ref, translation in zip(reference, translated):
+        print(ref)
+        print(translation)
     bleu = corpus_bleu(translated, reference)
-    print(reference)
-    print(translated)
     logging.info(f"BLEU score: {bleu.score}")
     return bleu.score
 
