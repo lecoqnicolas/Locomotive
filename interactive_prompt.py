aa5ccc08de473be9c199176dda4c57596e2e255c
Debian <debian@l40s-90-gra11-scalian-mce>
2024-10-30 15:14:17 +0000

    change device in config to 0 or 1 or cpu

--- interactive_prompt.py
+++ interactive_prompt.py
@@ -1,6 +1,6 @@
 import argparse
 from typing import NoReturn
-
+import time
 import torch
 
 from locomotive_llm.load import load_config
@@ -15,7 +15,7 @@ def main(params: argparse.Namespace) -> NoReturn:
     config = load_config(params.config, params.reverse)
     pipeline_class = get_pipeline(config)
     model = pipeline_class(model_id=config.llm_model,
-                              device="cuda" if torch.cuda.is_available() and not params.cpu else "cpu",
+                              device=config.device,
                               prompt_file=config.prompt,
                               batch_size=config.batch_size,
                               output_parser=config.response_parsing_method, prompt_ignore=config.ignore_prompt,
@@ -45,8 +45,11 @@ def main(params: argparse.Namespace) -> NoReturn:
             except KeyboardInterrupt:
                 print("")
                 exit(0)
-
+            start_time = time.time()
             translated_text = model.transform([text], config.src_name, config.tgt_name)
+            end_time = time.time()
+            response_time = end_time - start_time
+            print(f"Response time: {response_time:.4f} seconds")
             print(f"({config.tgt_code})> {translated_text[0]}")
 
 
@@ -60,9 +63,6 @@ if __name__ == "__main__":
     parser.add_argument('--reverse',
                         action='store_true',
                         help='Reverse the source and target languages in the configuration and data sources. Default: %(default)s')
-    parser.add_argument('--cpu',
-                        action="store_true",
-                        help='Force CPU use. Default: %(default)s')
     parser.add_argument('--gui', action="store_true", help="Enable gradio-powered gui", default=False)
     parser.add_argument('--manual_selection', action='store_true', help="Manual lang selection")
     args = parser.parse_args()
