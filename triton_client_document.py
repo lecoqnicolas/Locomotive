30138d4458b7e59446a191bf604761a3aca0c58c
maxime.carrere <maxime.carrere@scalian.com>
2024-11-06 15:22:57 +0100

    [FIX] document model batched

--- triton_client_document.py
+++ triton_client_document.py
@@ -42,29 +42,31 @@ def load_document(path):
 def main():
     client = tclient.InferenceServerClient(url="localhost:8001")
 
-    # Inputs
-    documents = [
-        "documents/input/DE_version_doc1.docx"
-    ]
     output_file = "translated_doc1.docx"
-    all_lines = []
-    doc_template = None
 
-    for doc_path in documents:
-        raw_content, doc_template = load_document(doc_path)
-        all_lines = [el["content"] for el in raw_content] if doc_path.endswith(('.docx', '.pdf')) else raw_content
+    doc_path = "documents/input/DE_version_doc1.docx"
+    raw_content, doc_template = load_document(doc_path)
+    all_lines = [el["content"] for el in raw_content] if doc_path.endswith(('.docx', '.pdf')) else raw_content
 
-    text_obj = np.array([all_lines], dtype="object")
+    text_obj = np.array(all_lines, dtype="object")
+    src_obj = np.array(["German" for _ in all_lines], dtype="object")
+    tgt_obj = np.array(["French" for _ in all_lines], dtype="object")
 
+    # Set Inputs
     input_tensors = [
-        tclient.InferInput("text_to_translate", text_obj.shape, np_to_triton_dtype(text_obj.dtype)),
-        tclient.InferInput("src_name", , np_to_triton_dtype(np.array([["German"]], dtype="object").dtype)),
-        tclient.InferInput("tgt_name", , np_to_triton_dtype(np.array([["French"]], dtype="object").dtype)),
+        tclient.InferInput(
+            "text_to_translate", text_obj.shape, np_to_triton_dtype(text_obj.dtype)
+        ),
+        tclient.InferInput(
+            "src_name", src_obj.shape, np_to_triton_dtype(text_obj.dtype)
+        ),
+        tclient.InferInput(
+            "tgt_name", tgt_obj.shape, np_to_triton_dtype(text_obj.dtype)
+        ),
     ]
-
     input_tensors[0].set_data_from_numpy(text_obj)
-    input_tensors[1].set_data_from_numpy(tgt_obj, dtype="object"))
-    input_tensors[2].set_data_from_numpy(dst_obj, dtype="object"))
+    input_tensors[1].set_data_from_numpy(src_obj)
+    input_tensors[2].set_data_from_numpy(tgt_obj)
 
     # Set outputs
     output = [tclient.InferRequestedOutput("translation")]
